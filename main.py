eafb2bcaf621c7fb682a68c4ac4d18fb2cff89b6
Белый Артем <wh1t3.art3m@mail.ru>
2022-12-06 23:27:50 +0300

    Добавлен файл main.py в верхний регистр

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import random
 
 def password():
     a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    b = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     pas = ''
     for i in range(11):
-        pas += random.choice(a)
+        pas += random.choice(a + b)
     return pas
 
 d = password()
